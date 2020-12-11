c38c6d7ee8e77ada171fa1e132892db21d316330
arn <arnaud.desaivres@fr.clara.net>
2020-12-11 13:02:55 +0100

    add comment in_code

--- sfx_wrapper.py
+++ sfx_wrapper.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# This script is a dirty hack in order to migrate from nagios to signalfx
+#
+# It will work with telegraf/exec
+# You can pass this script as the command
+# exemple :
+# ./sfx_wrapper.py --scriptname DNSMASQ --command "/usr/lib/nagios/plugins/check_dns -H www.google.fr -s localhost -w 1 -c 3"
+#
+# It create an event on signalfx which you can display on dashboards
+# You can then have the script output on your dashboard
 
 import subprocess
 from subprocess import Popen, PIPE
@@ -25,22 +34,33 @@ args = parser.parse_args()
 command_output = subprocess.Popen(args.command, shell = True, stdout=PIPE, stderr=PIPE)
 stdout, stderr = command_output.communicate()
 
+# We will need a temp file in order to verify if the event has already been sent.
+# if we do not store this, event will be sent on each execution with a return code != 0
+# temp folder
 cachedir = '/tmp/sfx_wrapper/'
+# Create it if it does not exist
 if not os.path.exists(cachedir):
     os.mkdir(cachedir)
+# cache file name construction
+# we need one for each test, so we will use the args provided to create the name
 cachefile = cachedir+args.eventtype+"_"+args.scriptname+".cache"
 
 # if command exit with 0 -> to nothing, just exit
 if command_output.returncode == 0:
+    # we remove the local trace of event send if it exist
+    # it will enable us to send an event again if the script fail
     if os.path.exists(cachefile):
         os.remove(cachefile)
 
+    # just print the script output
     print(stdout.decode("utf-8"))
+    # exit with command return code
     sys.exit(command_output.returncode)
 
 # if command exit is not 0 push payload to signalfx print error and exit with command exitcode
 if command_output.returncode != 0:
 
+    # Payload for signalfx
     SFX_ENDPOINT = "https://ingest."+args.realm+".signalfx.com/v2/event"
     sfx_payload = { 'category': '"'+args.category+'"',
             'eventType': args.eventtype,
@@ -55,6 +75,8 @@ if command_output.returncode != 0:
     sfx_headers = { 'Content-Type': 'application/json',
             'X-SF-Token': args.token,
             }
+    # if the cache file exist, event has already been sent
+    # so we just write the check timestamp to the file and exit with the script error code
     if os.path.exists(cachefile):
         f = open(cachefile, "w")
         f.write(str(time.time()))
@@ -62,7 +84,9 @@ if command_output.returncode != 0:
         print(stdout.decode("utf-8"))
         print(stderr.decode("utf-8"))
         sys.exit(command_output.returncode)
+    # if the cache file does not exist, we need to send the event to signalfx
     else:
+        # event sent to sfx : https://dev.splunk.com/observability/reference/api/ingest_data/latest#endpoint-send-custom-events
         sfx_event_request = requests.post(SFX_ENDPOINT, data = "["+json.dumps(sfx_payload)+"]", headers = sfx_headers)
         f = open(cachefile, "w")
         f.write(str(time.time()))
